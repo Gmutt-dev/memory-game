ac7f774f8160640a2dd8ee1c79452f8f91d42cca
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-10-06 12:32:38 +0200

    created GameBoard component and style files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import { useState } from "react";
 import "./App.css";
+import { GameBoard } from "./components/GameBoard";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Memory Game</h1>
+      <p>
+        Click on any card to begin. Cards will be reshuffled on each selection.
+        Do not select the same card twice!
+      </p>
+      <p>
+        One point is awarded per unique card clicked. Game restarts if the same
+        card is clicked twice.
+      </p>
+      <GameBoard />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,5 @@
+import "../styles/GameBoard.css";
+
+export function GameBoard() {
+  return <></>;
+}
