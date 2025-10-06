7ddbe00e703540e6415d9d0b1e79fd08585ae2d4
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-10-06 16:32:43 +0200

    fleshed out the first version of the PlayCard components

--- src/components/PlayCard.jsx
+++ src/components/PlayCard.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
 import "../styles/PlayCard.css";
 
-export function PlayCard() {
-  return <></>;
+export function PlayCard({ url, name, handlePlayCardClick }) {
+  const [imgUrl, setImgUrl] = useState();
+  const [loadStatus, setLoadStatus] = useState("loading");
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setImgUrl(data.sprites.front_shiny);
+        setLoadStatus("success");
+      })
+      .catch(() => setLoadStatus(`error`));
+  });
+
+  return (
+    <div className="playcard">
+      {(() => {
+        switch (loadStatus) {
+          case "loading":
+            return <p>"Loading..."</p>;
+          case "error":
+            return <p>"Error while loading"</p>;
+          case "success":
+            return (
+              <>
+                <img src={imgUrl} alt={`Image of ${name.toUpperCase()}`} />
+                <p>{name.toUpperCase()}</p>
+              </>
+            );
+          default:
+            return null;
+        }
+      })()}
+    </div>
+  );
 }
