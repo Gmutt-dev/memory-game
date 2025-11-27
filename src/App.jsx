2849342c503ef7cad3fe395c082862475ca2f0c7
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-11-27 14:41:19 +0200

    restyled and structured App and Gameboard headers

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import { GameBoard } from "./components/GameBoard";
 function App() {
   return (
     <div className="app">
-      <header className="header-app">
+      <header>
         <h1>Memory Game</h1>
-        <p>
-          Click on any card to begin. Cards will be reshuffled on each
-          selection. Do not select the same card twice!
-        </p>
-        <p>
-          One point is awarded per unique card clicked. Game restarts if the
-          same card is clicked twice.
+        <p className="instructions">
+          <span>Instructions:</span>
+          <ol className="instruction-list">
+            <li>Click on any card to begin.</li>
+            <li>Cards will be reshuffled on each selection.</li>
+            <li>Do not select the same card twice!</li>
+            <li>One point is awarded per unique card clicked.</li>
+            <li>Game restarts if the same card is clicked twice.</li>
+          </ol>
         </p>
       </header>
       <GameBoard />
