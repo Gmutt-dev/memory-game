4a38cf22da5cab60b4363fde47c7a2b16006171e
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-11-27 10:23:31 +0200

    bit of styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import { GameBoard } from "./components/GameBoard";
 function App() {
   return (
     <div className="app">
-      <h1>Memory Game</h1>
-      <p>
-        Click on any card to begin. Cards will be reshuffled on each selection.
-        Do not select the same card twice!
-      </p>
-      <p>
-        One point is awarded per unique card clicked. Game restarts if the same
-        card is clicked twice.
-      </p>
+      <header className="header-app">
+        <h1>Memory Game</h1>
+        <p>
+          Click on any card to begin. Cards will be reshuffled on each
+          selection. Do not select the same card twice!
+        </p>
+        <p>
+          One point is awarded per unique card clicked. Game restarts if the
+          same card is clicked twice.
+        </p>
+      </header>
       <GameBoard />
     </div>
   );
